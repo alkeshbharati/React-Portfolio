d6c7afb307c6b533da916e1b98650628887f47b5
Alkesh Bharati <alkeshbharati@MacBook-Pro-4.local>
2020-07-22 17:54:01 -0700

    Updates

---
+++ precache-manifest.ea52a6e85dc86f1ab638ad0f679d22c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a168e7f9a3f1ac50c555eecd41014426",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "f3aa65e0a61985616206",
+    "url": "/React-Portfolio/static/css/main.40c6db12.chunk.css"
+  },
+  {
+    "revision": "b97743b1076a4f90e2bf",
+    "url": "/React-Portfolio/static/js/2.8f18329a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.8f18329a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3aa65e0a61985616206",
+    "url": "/React-Portfolio/static/js/main.638736b3.chunk.js"
+  },
+  {
+    "revision": "7c35ff808cd94c4ce20c",
+    "url": "/React-Portfolio/static/js/runtime-main.0ad08de7.js"
+  }
+]);
